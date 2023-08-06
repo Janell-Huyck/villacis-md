cf7b2b5a585b49696153dfffef7423b17034835c
Janell Huyck <Janell.Huyck@outlook.com>
2023-08-06 14:03:43 -0400

    Initial commit from gatsby

---
+++ gatsby-config.js
@@ -0,0 +1,10 @@
+/**
+ * @type {import('gatsby').GatsbyConfig}
+ */
+module.exports = {
+  siteMetadata: {
+    title: `villacis md`,
+    siteUrl: `https://www.yourdomain.tld`,
+  },
+  plugins: [],
+}
