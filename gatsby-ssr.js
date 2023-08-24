e4f38be990377741a01fe9c4171bb56814558450
Janell Huyck <Janell.Huyck@outlook.com>
2023-08-24 12:34:42 -0400

    added static folder and the ability to have a favicon

--- gatsby-ssr.js
+++ gatsby-ssr.js
@@ -2,6 +2,19 @@
 // This will allow me to access the language context from anywhere in my application.
 // This is for server-side rendering.
 // gatsby-ssr.js
+import React from 'react';
 import { wrapRootElement } from './wrapRootElement';
-
 export { wrapRootElement };
+
+export const onRenderBody = ({ setHeadComponents }) => {
+  setHeadComponents([
+    <link
+      key="favicon"
+      rel="icon"
+      href="/favicon.svg"
+    />,
+  ]);
+};
+
+
+
